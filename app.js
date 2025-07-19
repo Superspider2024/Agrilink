31286aca595fd4b4b9072fa1b4d56f59541256fd
superspider2024 <diginalunlimited@gmail.com>
2025-07-19 21:54:25 +0300

    nice

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const bodyParser = require('body-parser');
 
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 const server = http.createServer(app); 
